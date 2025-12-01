3cdfbe2b2c134b641a820ccfcf11fbac7bc54b58
XEN STATHATOS <xenstath85@gmail.com>
2025-12-01 22:25:35 +0000

    Almost can add clients bit fucked still

--- WinFormsApp1/AdminForm.cs
+++ WinFormsApp1/AdminForm.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WinFormsApp1
 {
@@ -16,7 +17,42 @@ namespace WinFormsApp1
             this.Hide();
         }
 
-        
+        private void LoadGrid()
+        {
+            dgvClients.DataSource = null;
+            dgvClients.DataSource = clients;
+        }
+
+        private void btnAddClient_Click(object sender, EventArgs e)
+        {
+            Client client = new Client()
+            {
+                ClientID = txtClientID.Text,
+                ClientName = txtClientName.Text,
+                ClientAddress = txtClientAddress.Text,
+                ClientPhone = txtClientPhone.Text,
+
+
+            };
+
+            clients.Add(client);
+            LoadGrid();
+
+            MessageBox.Show("Client record added successfully!");
+
+        }
+
+        private void dgvClients_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtClientID.Text = dgvClients.Rows[e.RowIndex].Cells["ClientID"].Value.ToString();
+                txtClientName.Text = dgvClients.Rows[e.RowIndex].Cells["Name"].Value.ToString();
+                txtClientAddress.Text = dgvClients.Rows[e.RowIndex].Cells["Address"].Value.ToString();
+                txtClientPhone.Text = dgvClients.Rows[e.RowIndex].Cells["Phone"].Value.ToString();
+
+            }
+        }
     }
 }
 
---
+++ WinFormsApp1/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Client
+    {
+        public string ClientID { get; set; }
+        public string ClientName { get; set; }
+        public string ClientAddress { get; set; }
+        public string ClientPhone { get; set; }
+        
+    }
+}
